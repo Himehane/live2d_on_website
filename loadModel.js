c8f66f39e4accc7dc5a4fb2449e7093c5152648f
Himehane <370252030@qq.com>
2019-01-08 15:23:34 +0800

    调整优化加载方式

--- loadModel.js
+++ loadModel.js
@@ -1,25 +1,28 @@
-var LIVE2DCUBISMCORE =Live2DCubismCore
+var LIVE2DCUBISMCORE = Live2DCubismCore
 //如果资源在CDN，一定要写http://或者https://否则会以本域名请求且为相对路径
-var themePath = window.location.protocol+'//'+ window.location.host+"/Resource/../..";
+var themePath = window.location.protocol+'//'+ window.location.host+"/live2d_on_websit/Resource/../..";
 //模型的model3.json文件
-var modelPath = themePath + 'your model3.json file';
+var modelPath = themePath + '/' + 'your model3.json file';
 //模型渲染的位置
 var tag_target = '.waifu';
 //待机的动作索引
 var idleIndex;
-//登录的动作索引
+//登录的动作索引，只针对动作文件中有idel字段的
 var loginIndex;
+//回港动作，只针对碧蓝航线等有回港动作，动作文件中有home字段
+var homeIndex;
+//模型偏移位置
+var model_x = -25;
+var model_y = 40;
 //渲染模型的宽高
-var modelWidth = 280;
-var modelHight = 250;
+var modelWidth = 360;
+var modelHight = 320;
 //渲染模型的比例
-var scale = 17.5;
+var scale = 22;
 //动作总数
 var motionCount = 0 ;
 //测试用，加载时间起点，不保证准确性
 var startTime = new Date().getTime();
-//动作数组，存放格式化好的动作数据
-var motions = [];
 //第一种方式初始化模型，通过model3.json的内容去导入
 function initModelConfig(modelJson){
     var fileReferences = modelJson.FileReferences;
@@ -28,18 +31,16 @@ function initModelConfig(modelJson){
     var textures = fileReferences.Textures;
     loadTextures(textures);
     var phyPath = fileReferences.Physics;
+    loadPhyPath(phyPath);
     for (const key in fileReferences.Motions) {
-        motions =  fileReferences.Motions[key];
-        console.log(motions);
+        loadMotions(fileReferences.Motions[key]);
     }
-    PIXI.loader.load(function (loader, resources) {
-        var app = new PIXI.Application(modelWidth, modelHight, { transparent: true });
+    PIXI.loader.on("progress", loadProgressHandler).load(function (loader, resources) {
         var canvas = document.querySelector(tag_target);
-        canvas.appendChild(app.view);
-        canvas.querySelector('canvas').setAttribute('id','live2d');
+        var view = canvas.querySelector('canvas');
+        var app = new PIXI.Application(modelWidth, modelHight, {transparent: true ,view: view});
         var moc = Live2DCubismCore.Moc.fromArrayBuffer(resources['moc'].data);
         var builder = new LIVE2DCUBISMPIXI.ModelBuilder();
-        
         builder.setMoc(moc);
         builder.setTimeScale(1);
         var textureIndex = 0;
@@ -52,18 +53,18 @@ function initModelConfig(modelJson){
         var model = builder.build();
         app.stage.addChild(model);
         app.stage.addChild(model.masks);
-        setMotions(model,resources);
-        setMouseTrick(model,app,canvas);
+        var motions = setMotions(model,resources);
+        setMouseTrick(model,app,canvas,motions);
         var onResize = function (event) {
             if (event === void 0) { event = null; }
-            var width = modelWidth;
-            var height = modelHight;
-            app.view.style.width = width + "px";
-            app.view.style.height = height + "px";
-            app.renderer.resize(width, height);    
-            model.position = new PIXI.Point(modelWidth/2, modelHight/2);
-            model.scale = new PIXI.Point(scale, scale);
-            model.masks.resize(app.view.width, app.view.height);
+                var width = modelWidth;
+                var height = modelHight;
+                app.view.style.width = width + "px";
+                app.view.style.height = height + "px";
+                app.renderer.resize(width, height);
+                model.position = new PIXI.Point(modelWidth/2 + model_x, modelHight/2 + model_y);
+                model.scale = new PIXI.Point(scale, scale);
+                model.masks.resize(app.view.width, app.view.height);
         };
         onResize();
         window.onresize = onResize;
@@ -81,7 +82,6 @@ function loadMoc(mocPath){
 function loadTextures(textures){
     if(textures.length >0){
         for (let i = 0; i < textures.length; i++) {
-            console.log(textures[i]);
             //loadTextures;
             PIXI.loader.add('texture' + ( i + 1) , themePath + textures[i]);
         }
@@ -102,7 +102,13 @@ function loadMotions(motions){
     if(motions.length >0){
         for (let i = 0; i < motions.length; i++) {
             PIXI.loader.add('motion'+ ( motionCount + 1) , themePath + motions[i].File, { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON });
-            if(motions[i].File.indexOf('idle')!= -1){idleIndex = motionCount;}else if(motions[i].File.indexOf('login') != -1){loginIndex = motionCount;}
+            if(motions[i].File.indexOf('idle')!= -1){
+                idleIndex = motionCount;
+            }else if(motions[i].File.indexOf('login') != -1){
+                loginIndex = motionCount;
+            }else if(motions[i].File.indexOf('home') != -1){
+                homeIndex = motionCount;
+            }
             motionCount ++ ;
         }
     }else{
@@ -110,7 +116,7 @@ function loadMotions(motions){
     }
 }
 //简单发送AJAX异步请求读取json文件
-function ajax(url){
+function loadModel(url){
     var ajax = null;
     if(window.XMLHttpRequest){ajax = new XMLHttpRequest();}else if(window.ActiveObject){
         ajax = new ActiveXObject("Microsoft.XMLHTTP");
@@ -131,11 +137,10 @@ function ajax(url){
         }
     };
 }
-ajax(modelPath);
 //另一种初始化模型方式
 function initModel(data){
     var model3Obj = {data:data,url:themePath};
-    var loader =PIXI.loader;
+    var loader =PIXI.loader.on("progress", loadProgressHandler);
     for (const key in data.FileReferences.Motions) {
         loadMotions(data.FileReferences.Motions[key]);
     }
@@ -144,26 +149,31 @@ function initModel(data){
 }
 //设置模型的回调方法
 function setModel(model){
-    var app = new PIXI.Application(modelWidth, modelHight, { transparent: true });
     var canvas = document.querySelector(tag_target);
-    canvas.appendChild(app.view);
-    canvas.querySelector('canvas').setAttribute('id','live2d');
+    var view = canvas.querySelector('canvas');
+    var app = new PIXI.Application(modelWidth, modelHight, {transparent: true ,view:view});
     app.stage.addChild(model);
     app.stage.addChild(model.masks);
-    var resources = PIXI.loader.resources;
-    setMotions(model,resources);
-    setMouseTrick(model,app,canvas);
-    app.view.style.width = modelWidth + "px";
-    app.view.style.height = modelHight + "px";
-    app.renderer.resize(modelWidth, modelHight);    
-    model.position = new PIXI.Point(modelWidth/2, modelHight/2);
-    model.scale = new PIXI.Point(scale, scale);
-    model.masks.resize(app.view.width, app.view.height);
-    loadTime = new Date().getTime() - startTime;
-    console.log('Model initialized in '+ loadTime/1000 + ' second');
+    var motions = setMotions(model,PIXI.loader.resources);
+    setMouseTrick(model,app,canvas,motions);
+    var onResize = function (event) {
+        if (event === void 0) { event = null; }
+            var width = modelWidth;
+            var height = modelHight;
+            app.view.style.width = width + "px";
+            app.view.style.height = height + "px";
+            app.renderer.resize(width, height);
+            model.position = new PIXI.Point(modelWidth/2 + model_x, modelHight/2 + model_y);
+            model.scale = new PIXI.Point(scale, scale);
+            model.masks.resize(app.view.width, app.view.height);
+    };
+    onResize();
+    window.onresize = onResize;
 }
 //设置模型动作
 function setMotions(model,resources){
+    //动作数组，存放格式化好的动作数据
+    var motions = [];
     for (const key in resources) {
         if(key.indexOf('motion') != -1){
             motions.push(LIVE2DCUBISMFRAMEWORK.Animation.fromMotion3Json(resources[key].data)); 
@@ -173,12 +183,18 @@ function setMotions(model,resources){
     if(motions.length > 0){
         window.clearTimeout(timeOut);
         model.animator.addLayer("motion", LIVE2DCUBISMFRAMEWORK.BuiltinAnimationBlenders.OVERRIDE, 1.0);
-        model.animator.getLayer("motion").play(motions[loginIndex]);
-        timeOut = setTimeout( function(){model.animator.getLayer("motion").play(motions[idleIndex]);}, motions[loginIndex].duration * 1000 );
+        if(null != loginIndex && null != idleIndex){//如果有登录和待机动作，则在登录动作完成后切换到待机动作
+            model.animator.getLayer("motion").play(motions[loginIndex]);
+            timeOut = setTimeout( function(){model.animator.getLayer("motion").play(motions[idleIndex]);}, motions[loginIndex].duration * 1000 );
+        }else{
+            //如果没有登录动作，则默认播放第一个动作
+            model.animator.getLayer("motion").play(motions[0]);
+        }
     }
+    return motions;
 }
 //设置鼠标追击
-function setMouseTrick(model,app,canvas){
+function setMouseTrick(model,app,canvas,motions){
     var rect = canvas.getBoundingClientRect();
     var center_x = modelWidth/2 + rect.left, center_y = modelHight/2 + rect.top;
     var mouse_x = center_x, mouse_y = center_y;
@@ -218,13 +234,47 @@ function setMouseTrick(model,app,canvas){
             var rand = Math.floor(Math.random() * motions.length);
             model.animator.getLayer("motion").stop();
             model.animator.getLayer("motion").play(motions[rand]);
-            timeOut = setTimeout( function(){model.animator.getLayer("motion").play(motions[idleIndex]);}, motions[rand].duration * 1000 );
+            //如果有登录动作，则在随机播放动作结束后回到待机动作
+            if(null != idleIndex){
+                timeOut = setTimeout( function(){model.animator.getLayer("motion").play(motions[idleIndex]);}, motions[rand].duration * 1000 );
+            }
         }
     });
+    var onblur = false;
+    var onfocusTime;
+    sessionStorage.setItem('Onblur', '0');
+    window.onblur = function(e){
+        if('0' == sessionStorage.getItem('Onblur')){
+            onfocusTime = setTimeout(function(){sessionStorage.setItem('Onblur','1');},30000);
+        }
+    };
+    window.onfocus = function(e){
+        window.clearTimeout(onfocusTime);
+        if(motions.length > 0){
+            if('1' == sessionStorage.getItem('Onblur')){
+                model.animator.getLayer("motion").stop();
+                if(null != loginIndex && null != idleIndex){//如果有回港和待机动作，则在登录动作完成后切换到待机动作
+                    model.animator.getLayer("motion").play(motions[homeIndex]);
+                    onfocusTime = setTimeout( function(){model.animator.getLayer("motion").play(motions[idleIndex]);sessionStorage.setItem('Onblur', '0');}, motions[homeIndex].duration * 1000 );
+                }else{
+                    //如果没有，则默认播放第一个动作
+                    model.animator.getLayer("motion").play(motions[0]);
+                }
+            }
+        }
+    };
 }
 //获取页面内容方法
 function bodyOrHtml(){
     if('scrollingElement' in document){ return document.scrollingElement; }
     if(navigator.userAgent.indexOf('WebKit') != -1){ return document.body; }
     return document.documentElement;
+}
+//加载模型Handler，监控加载进度
+function loadProgressHandler(loader) {
+    console.log("progress: " + Math.round(loader.progress) + "%");
+    if(loader.progress >= 100){ 
+        var loadTime = new Date().getTime() - startTime;
+        console.log('Model initialized in '+ loadTime/1000 + ' second');
+    }
 }
